8c2de58d5c574e1a17e322519f00850227eb5bd1
SABARI YOGESH S <sabariyogesh.s32@gmail.com>
2026-06-09 03:17:55 +0530

    Added UART RX/TX project

---
+++ UART/8_USART_RXTX/Src/main.c
@@ -0,0 +1,58 @@
+/* Firmware Driver for USART2*/
+#include "stm32f446xx.h"
+#include <stdint.h>
+#include <stdio.h>
+#include "uart.h"
+#define GPIOA_EN	(1U<<0)
+#define PA5	        (1U<<5)
+#define LED_PIN	     PA5
+
+char key;
+int main(void)
+{
+	RCC->AHB1ENR |= GPIOA_EN;
+	GPIOA->MODER |= (1U<<10);
+	GPIOA->MODER &= ~(1U<<11);
+	uart2_rxtx_init();
+
+    while(1)
+    {
+    	key=uart2_read();
+
+
+    if(key == 'L')
+    {
+    	GPIOA->ODR |= LED_PIN;
+    }
+    else
+    {
+    	GPIOA->ODR &= ~LED_PIN;    }
+    }
+}
+
+
+/*
+ * #include "stm32f446xx.h"
+#define GPIOAEN 	(1U<<0)
+#define PIN5 	(1U<<5)
+#define LED_PIN	PIN5
+int main(void)
+{
+
+		RCC->AHB1ENR |=GPIOAEN;
+		GPIOA->MODER |=(1U<<10);
+		GPIOA->MODER &=~(1U<<11);
+
+    while(1)
+    {
+    	 GPIOA->ODR ^= LED_PIN;
+    	for(int i=0;i<100000;i++)
+    	{
+
+    	}
+
+    }
+}
+ *
+ */
+
